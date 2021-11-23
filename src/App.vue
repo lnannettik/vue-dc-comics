21fa7258ad463f08eb938c4a5f5629a9b84b0a69
lnannettik <l.nannetti.k@gmail.com>
2021-11-23 17:31:35 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
   <div id="app">
-    My vue APP
+    <!-- HEADER -->
+    <Header />
+
+    <!-- MAIN -->
+    <main></main>
+
+    <!-- FOOTER -->
+    <footer></footer>
   </div>
 </template>
 
 <script>
-
+import Header from '@/components/Header.vue'; 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    Header,
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  /* GENERAL RESET */
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+
+  img {
+    max-width: 100%;
+
+  }
+
+
+  // UTILITIES
+  .container {
+    max-width: 1170px;
+    margin: 0 auto;
+    padding: 0 16px;
+
+  }
+
+  .text-center {
+    text-align: center;
+  }
+
+
+
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <h1>Header....here</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
